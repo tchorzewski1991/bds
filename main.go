bb7c2062984a88a26f0cf00d53bc200f5612908f
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-02-20 12:29:15 +0100

    Update main with base setup for running in k8s env

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/emadolsky/automaxprocs/maxprocs"
+	_ "go.uber.org/automaxprocs"
+)
+
+var build = "develop"
+var service = "flights-api"
 
 func main() {
-	fmt.Println("FDS")
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		fmt.Println("maxprocs: %w", err)
+		os.Exit(1)
+	}
+
+	cpu := runtime.GOMAXPROCS(0)
+
+	log.Printf("Starting service[%s] build[%s] cpu[%d]\n", service, build, cpu)
+	defer log.Printf("Stopped service[%s] build[%s] cps[%d]\n", service, build, cpu)
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+	<-shutdown
+
+	log.Printf("Stopping service[%s] build[%s] cpu[%d]\n", service, build, cpu)
 }
