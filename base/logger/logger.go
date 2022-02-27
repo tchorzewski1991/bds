6eb570bf08a46041967efcc1085630acd4a5db7e
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-02-27 12:52:44 +0100

    Add simple abstraction for app logger

---
+++ base/logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import "go.uber.org/zap"
+
+func New(fields ...Field) (*zap.SugaredLogger, error) {
+	conf := zap.NewProductionConfig()
+	conf.DisableStacktrace = true
+	conf.OutputPaths = []string{"stdout"}
+
+	// Assign initial fields common to all of logs
+	initialFields := make(map[string]interface{})
+	for _, f := range fields {
+		initialFields[f.Name] = f.Value
+	}
+	conf.InitialFields = initialFields
+
+	// Build logger from config
+	logger, err := conf.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return logger.Sugar(), nil
+}
+
+type Field struct {
+	Name  string
+	Value interface{}
+}
