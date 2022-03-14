b4ae20031c8cf66844bafa2f7b82552dda6eed96
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-03-14 20:03:12 +0100

    Add initial code for v2 flights list handler

--- app/services/flights-api/handlers/v2/v2.go
+++ app/services/flights-api/handlers/v2/v2.go
@@ -1,13 +1,19 @@
 package v2
 
 import (
+	fh "github.com/tchorzewski1991/fds/app/services/flights-api/handlers/v2/flight"
 	"github.com/tchorzewski1991/fds/base/web"
 	"go.uber.org/zap"
+	"net/http"
 )
+
+const version = "v2"
 
 type Config struct {
 	Logger *zap.SugaredLogger
 }
 
-// Routes binds all the version 2 routes
-func Routes(app *web.App, cfg Config) {}
+// Routes binds all the routes for API version 2
+func Routes(app *web.App, _ Config) {
+	app.Handle(http.MethodGet, version, "/flights", fh.List)
+}
