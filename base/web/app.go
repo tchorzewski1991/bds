9151340a329f4afd283b485352b187f172e6b834
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-03-14 19:59:23 +0100

    Extend App with support for handling request with additional ctx

--- base/web/app.go
+++ base/web/app.go
@@ -1,11 +1,16 @@
 package web
 
 import (
+	"context"
 	"github.com/dimfeld/httptreemux/v5"
 	"net/http"
 	"os"
 	"syscall"
 )
+
+// Handler represents type responsible for handling http request.
+// It extends signature of http.HandlerFunc with support for context.Context.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 type App struct {
 	mux      *httptreemux.ContextMux
@@ -25,4 +30,21 @@ func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) Shutdown() {
 	a.shutdown <- syscall.SIGTERM
+}
+
+func (a *App) Handle(method string, version string, path string, handler Handler) {
+	// Prepare the function to execute for each request.
+	// This anonymous func wraps Handler with proper error handling.
+	h := func(w http.ResponseWriter, r *http.Request) {
+		if err := handler(r.Context(), w, r); err != nil {
+			a.Shutdown()
+			return
+		}
+	}
+	// Extend path with version if necessary.
+	if version != "" {
+		path = "/" + version + path
+	}
+	// Register handler func with the requested method and path.
+	a.mux.Handle(method, path, h)
 }
