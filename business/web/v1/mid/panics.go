d3eb096b430b3828d6e9747b8c3a5c0eeedfcf7a
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-03-20 13:57:30 +0100

    Add implementation for v1 panics middleware

---
+++ business/web/v1/mid/panics.go
@@ -0,0 +1,40 @@
+package mid
+
+import (
+	"context"
+	"fmt"
+	"github.com/tchorzewski1991/fds/base/web"
+	"net/http"
+	"runtime/debug"
+)
+
+// Panics recovers from panic() and captures the error, so it is reported in Errors middleware.
+func Panics() web.Middleware {
+
+	// m is the middleware function to be executed.
+	m := func(handler web.Handler) web.Handler {
+
+		// h is the handler that will be attached in the middleware chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
+
+			// How it works?
+			// Defer runs in between state between function returning and calling function gaining back control.
+			// When the panic happens defer + recover is able to overwrite err according to predefined guidelines.
+			defer func() {
+				if rec := recover(); rec != nil {
+					// Stack trace will be provided.
+					trace := debug.Stack()
+					err = fmt.Errorf("PANIC [%v] TRACE[%s]", rec, string(trace))
+				}
+			}()
+
+			// Call the next handler. Err variable will be set automatically as it is declared
+			// on the method signature level.
+			return handler(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return m
+}
