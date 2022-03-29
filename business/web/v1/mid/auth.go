ee67c86da71cbb18fc8aaa3e0073093f26d22922
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-03-29 21:02:57 +0200

    Add implementation for v1 auth middleware

---
+++ business/web/v1/mid/auth.go
@@ -0,0 +1,47 @@
+package mid
+
+import (
+	"context"
+	"errors"
+	"github.com/tchorzewski1991/fds/base/web"
+	"github.com/tchorzewski1991/fds/business/sys/auth"
+	v1 "github.com/tchorzewski1991/fds/business/web/v1"
+	"net/http"
+	"strings"
+)
+
+func Auth() web.Middleware {
+
+	// m is the middleware function to be executed.
+	m := func(handler web.Handler) web.Handler {
+
+		// h is the handler that will be attached in the middleware chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				err := errors.New("authorization header is not set")
+				return v1.NewRequestError(err, http.StatusUnauthorized)
+			}
+
+			parts := strings.Split(authHeader, " ")
+			if len(parts) < 2 && strings.ToLower(parts[0]) != "bearer" {
+				err := errors.New("authorization header has invalid format. Expected: Bearer TOKEN")
+				return v1.NewRequestError(err, http.StatusUnauthorized)
+			}
+
+			claims, err := auth.ValidateToken(parts[1])
+			if err != nil {
+				return v1.NewRequestError(err, http.StatusUnauthorized)
+			}
+
+			ctx = auth.SetClaims(ctx, claims)
+
+			return handler(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return m
+}
