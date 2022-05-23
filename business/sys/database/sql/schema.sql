9c32fb2e261baba5c99a7ac89162a6d6316f1a00
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-05-23 07:35:59 +0200

    Update db schema with initial version of books table

--- business/sys/database/sql/schema.sql
+++ business/sys/database/sql/schema.sql
@@ -15,3 +15,18 @@ CREATE TABLE users (
 -- Version: 1.2
 -- Description: Remove column name from users
 ALTER TABLE users DROP COLUMN name;
+
+-- Version: 1.3
+-- Description: Create table books
+CREATE TABLE books (
+   id               SERIAL,
+   isbn             TEXT NOT NULL CHECK (isbn <> ''),
+   title            TEXT NOT NULL CHECK (title <> ''),
+   author           TEXT,
+   publication_year TEXT,
+   publisher        TEXT,
+   created_at       TIMESTAMP DEFAULT now(),
+   updated_at       TIMESTAMP DEFAULT now(),
+
+   PRIMARY KEY (id)
+);
