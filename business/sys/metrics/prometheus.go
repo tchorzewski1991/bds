a216126ab4f5a9514410814bff27ea9b08ce2374
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-05-30 20:50:06 +0200

    Fix bug in prom. metrics by moving the moment of reading label values to Send() method

--- business/sys/metrics/prometheus.go
+++ business/sys/metrics/prometheus.go
@@ -25,50 +25,66 @@ func init() {
 	prometheus.Register(httpHist)
 }
 
-type LabelValuesFunc func() []string
-
 type Histogram struct {
-	hist       *prometheus.HistogramVec
-	valuesFunc LabelValuesFunc
-	begin      time.Time
+	hist  *prometheus.HistogramVec
+	lvr   labelValuesReader
+	begin time.Time
 }
 
 func (h *Histogram) Send() {
 	took := time.Since(h.begin)
-	lvs := h.valuesFunc()
+	lvs := h.lvr.Read()
 	h.hist.WithLabelValues(lvs...).Observe(took.Seconds())
 }
 
 // HttpHistogram creates a new Histogram responsible for observing http requests
 func HttpHistogram(r *http.Request, v *web.CtxValues) *Histogram {
 	return &Histogram{
-		hist:       httpHist,
-		valuesFunc: HTTPLabelValues(r, v),
-		begin:      time.Now(),
-	}
-}
-
-func HTTPLabelValues(r *http.Request, v *web.CtxValues) LabelValuesFunc {
-	return func() []string {
-		handler := r.URL.Path
-		method := r.Method
-		code := strconv.Itoa(v.StatusCode)
-
-		return []string{handler, method, code}
+		hist: httpHist,
+		lvr: &httpLabelValues{
+			r: r,
+			v: v,
+		},
+		begin: time.Now(),
 	}
 }
 
 // DbHistogram creates a new Histogram responsible for observing db calls
 func DbHistogram(table, operation string) *Histogram {
 	return &Histogram{
-		hist:       dbHist,
-		valuesFunc: DBLabelValues(table, operation),
-		begin:      time.Now(),
+		hist: dbHist,
+		lvr: &dbLabelValues{
+			table:     table,
+			operation: operation,
+		},
+		begin: time.Now(),
 	}
 }
 
-func DBLabelValues(table, operation string) LabelValuesFunc {
-	return func() []string {
-		return []string{table, operation}
-	}
+// private
+
+type labelValuesReader interface {
+	Read() []string
+}
+
+type httpLabelValues struct {
+	r *http.Request
+	v *web.CtxValues
+}
+
+func (v *httpLabelValues) Read() []string {
+	handler := v.r.URL.Path
+	method := v.r.Method
+	code := strconv.Itoa(v.v.StatusCode)
+
+	return []string{handler, method, code}
+}
+
+type dbLabelValues struct {
+	table     string
+	operation string
+}
+
+func (v *dbLabelValues) Read() []string {
+	return []string{v.table, v.operation}
 }
